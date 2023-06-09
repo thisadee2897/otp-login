d9efcc1ea6a32ba01a54556511c10ad249db824b
odin.tch <thisadee.burom@gmail.com>
2023-06-09 14:20:42 +0700

    WIP UI Login

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,15 +1,16 @@
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../componemts/AuthContext';
-import '../css/Login.css'
+import '../css/LoginFacebook.css';
+
 export default function Login() {
     const navigate = useNavigate();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('admin');
+    const [password, setPassword] = useState('adminadminadminadminadminadmin');
     const { setIsLoggedIn } = useContext(AuthContext);
 
     const handleLogin = async () => {
-        if (username === 'odin.dev' && password === 'adminadmin') {
+        if (username === 'admin' && password === 'adminadminadminadminadminadmin') {
             setIsLoggedIn(true);
             navigate('/');
         } else {
@@ -18,24 +19,22 @@ export default function Login() {
     };
 
     return (
-        <div className="container">
-            <div className="card">
-                <h2 className="heading">Login</h2>
+        <div className="login-container">
+            <div className="login-form">
+                <h2>Login</h2>
                 <input
-                    className="input"
                     type="text"
                     placeholder="Username"
                     value={username}
                     onChange={e => setUsername(e.target.value)}
                 />
                 <input
-                    className="input"
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                 />
-                <button className="button" onClick={handleLogin}>
+                <button onClick={handleLogin}>
                     Login
                 </button>
             </div>
