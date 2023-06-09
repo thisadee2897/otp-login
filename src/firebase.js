f2656a56ff9f80fa85b47841786172ffeff0e91e
odin.tch <thisadee.burom@gmail.com>
2023-06-09 17:07:21 +0700

    WIP FIREBASE

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyBTR3mZ-eEJUDmeQ1WjD0Ga-H6NrKSts_0",
+    authDomain: "react-otp-77244.firebaseapp.com",
+    projectId: "react-otp-77244",
+    storageBucket: "react-otp-77244.appspot.com",
+    messagingSenderId: "678844699079",
+    appId: "1:678844699079:web:1abfc42b7d0bc0afc609e2"
+}
+firebase.initializeApp(config);
+export default firebase
