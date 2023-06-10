9ca4f18fdfc1fd431b2c62790c981bd0c764e687
odin.tch <thisadee.burom@gmail.com>
2023-06-10 11:05:26 +0700

    WIP โรพำิฟหำ

--- src/componemts/OTP.js
+++ src/componemts/OTP.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../css/OTPForm.css';
+import firebase from './firebase';
 
 export default function OTPForm() {
     const [otp, setOTP] = useState(['', '', '', '', '', '']);
@@ -9,7 +10,7 @@ export default function OTPForm() {
         const updatedOTP = [...otp];
         updatedOTP[index] = value;
         setOTP(updatedOTP);
-
+        console.log(updatedOTP);
         if (value.length === 1 && index < 5) {
             const nextInput = event.target.nextElementSibling;
             if (nextInput) {
@@ -20,8 +21,24 @@ export default function OTPForm() {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // ตรวจสอบ OTP ที่ป้อนข้อมูลที่นี่
-        console.log(otp.join('')); // แสดงค่า OTP ที่เก็บไว้ในรูปแบบสตริง
+        let recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
+        let phoneNumber = '+66' + otp.slice(1).join('');
+        firebase
+            .auth()
+            .signInWithPhoneNumber(phoneNumber, recaptcha)
+            .then(function (e) {
+                let code = prompt('Enter the OTP', '');
+                if (code == null) return;
+                e.confirm(code)
+                    .then(function (result) {
+                        document.querySelector('label').textContent =
+                            result.user.phoneNumber + ' Number verified';
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            });
+        console.log(otp.join(''));
     };
 
     return (
@@ -29,20 +46,54 @@ export default function OTPForm() {
             <h2>ยืนยัน OTP</h2>
             <form className="otp-form" onSubmit={handleSubmit}>
                 <div className="otp-inputs">
-                    {otp.map((value, index) => (
-                        <input
-                            key={index}
-                            type="text"
-                            maxLength="1"
-                            value={value}
-                            onChange={(e) => handleChange(e, index)}
-                        />
-                    ))}
+                    <input
+                        key={0}
+                        type="text"
+                        maxLength="1"
+                        value={otp[0]}
+                        onChange={(e) => handleChange(e, 0)}
+                    />
+                    <input
+                        key={1}
+                        type="text"
+                        maxLength="1"
+                        value={otp[1]}
+                        onChange={(e) => handleChange(e, 1)}
+                    />
+                    <input
+                        key={2}
+                        type="text"
+                        maxLength="1"
+                        value={otp[2]}
+                        onChange={(e) => handleChange(e, 2)}
+                    />
+                    <input
+                        key={3}
+                        type="text"
+                        maxLength="1"
+                        value={otp[3]}
+                        onChange={(e) => handleChange(e, 3)}
+                    />
+                    <input
+                        key={4}
+                        type="text"
+                        maxLength="1"
+                        value={otp[4]}
+                        onChange={(e) => handleChange(e, 4)}
+                    />
+                    <input
+                        key={5}
+                        type="text"
+                        maxLength="1"
+                        value={otp[5]}
+                        onChange={(e) => handleChange(e, 5)}
+                    />
                 </div>
                 <button type="submit" className="otp-submit-btn">
                     ยืนยัน
                 </button>
             </form>
+            <div id="recaptcha"></div>
         </div>
     );
 }
--- src/componemts/firebase.js
+++ src/componemts/firebase.js
@@ -1,11 +1,17 @@
-import firebase from 'firebase'
-const config = {
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
     apiKey: "AIzaSyBTR3mZ-eEJUDmeQ1WjD0Ga-H6NrKSts_0",
     authDomain: "react-otp-77244.firebaseapp.com",
     projectId: "react-otp-77244",
     storageBucket: "react-otp-77244.appspot.com",
     messagingSenderId: "678844699079",
     appId: "1:678844699079:web:1abfc42b7d0bc0afc609e2"
-}
-firebase.initializeApp(config);
-export default firebase
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
+export default app;
